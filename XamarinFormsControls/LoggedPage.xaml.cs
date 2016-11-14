60427f0352226f8a2735b379c310cd6c5783e788
zmija1994 <zzmija1@gmail.com>
2016-11-14 17:27:05 +0100

    Initial check-in of module XamarinFormsControls

---
+++ XamarinFormsControls/LoggedPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsControls
+{
+	public partial class LoggedPage : ContentPage
+	{
+		public LoggedPage()
+		{
+			InitializeComponent();
+
+			Content = new StackLayout()
+			{
+				Children =
+				{
+					new Label
+					{
+						Text = "Adam zaraz Ci pomoge",
+						Margin = new Thickness(0,50,0,0),
+						HorizontalOptions = LayoutOptions.Center
+					},
+					button1
+				}
+			};
+
+			button1.Clicked += (sender, e) =>
+			{
+				Navigation.PopModalAsync();
+			};
+
+
+		}
+
+		Button button1 = new Button
+		{
+			BackgroundColor = Color.Red,
+			Text = "POWROT"
+		};
+	}
+}
---
+++ XamarinFormsControls/XamarinFormsControlsPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsControls
+{
+	public partial class XamarinFormsControlsPage : ContentPage
+	{
+		public XamarinFormsControlsPage()
+		{
+			InitializeComponent();
+
+			BackgroundColor = Color.Gray;
+			Content = new StackLayout()
+			{
+				Children = 
+				{
+					label1,
+					loginEditor,
+					label2,
+					passwordEditor,
+					button1
+				}
+			};
+
+			button1.Clicked += (sender, e) =>
+			{
+				if (loginEditor.Text.Equals("Admin"))
+				{
+					if (passwordEditor.Text.Equals("Admin"))
+					{
+						Navigation.PushModalAsync(new LoggedPage());
+					}
+
+				}
+			};
+		}
+
+		Button button1 = new Button()
+		{
+			BackgroundColor = Color.Olive,
+			Text = "Zaloguj sie",
+			TextColor = Color.White
+		};
+
+		Editor loginEditor = new Editor()
+		{
+		};
+
+		Editor passwordEditor = new Editor()
+		{
+		};
+
+		Label label1 = new Label()
+		{
+			Text = "Login",
+			Margin = new Thickness(0,50,0,0),
+			HorizontalOptions = LayoutOptions.Center
+		};
+
+		Label label2 = new Label()
+		{
+			Text = "Haslo",
+			HorizontalOptions = LayoutOptions.Center
+		};
+
+
+
+	}
+}
